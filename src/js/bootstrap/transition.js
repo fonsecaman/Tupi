60d50a6bd4b3224752373cac450cc7582824b6e0
Daniel Torres <danielfeelfine@gmail.com>
2014-04-15 14:06:39 -0300

    Update src/bootstrap/transition.js
    pass in initial jshint test

--- src/js/bootstrap/transition.js
+++ src/js/bootstrap/transition.js
@@ -14,35 +14,35 @@
   // ============================================================
 
   function transitionEnd() {
-    var el = document.createElement('bootstrap')
+    var el = document.createElement('bootstrap');
 
     var transEndEventNames = {
       'WebkitTransition' : 'webkitTransitionEnd',
       'MozTransition'    : 'transitionend',
       'OTransition'      : 'oTransitionEnd otransitionend',
       'transition'       : 'transitionend'
-    }
+    };
 
     for (var name in transEndEventNames) {
       if (el.style[name] !== undefined) {
-        return { end: transEndEventNames[name] }
+        return { end: transEndEventNames[name] };
       }
     }
 
-    return false // explicit for ie8 (  ._.)
+    return false; // explicit for ie8 (  ._.)
   }
 
   // http://blog.alexmaccaw.com/css-transitions
   $.fn.emulateTransitionEnd = function (duration) {
-    var called = false, $el = this
-    $(this).one($.support.transition.end, function () { called = true })
-    var callback = function () { if (!called) $($el).trigger($.support.transition.end) }
-    setTimeout(callback, duration)
-    return this
-  }
+    var called = false, $el = this;
+    $(this).one($.support.transition.end, function () { called = true; });
+    var callback = function () { if (!called) $($el).trigger($.support.transition.end); };
+    setTimeout(callback, duration);
+    return this;
+  };
 
   $(function () {
-    $.support.transition = transitionEnd()
-  })
+    $.support.transition = transitionEnd();
+  });
 
 }(jQuery);
